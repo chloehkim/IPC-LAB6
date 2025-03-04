4063d3766a27ced5b244cbd5a50d52511c68a356
chloehkim <chloekim6156@gmail.com>
2025-03-04 18:37:26 +0000

    %

---
+++ CLASS.c/Class.c
@@ -0,0 +1,23 @@
+#incloud <stdio.h>
+
+struct Student
+{
+    int stduentNum;
+    float grades[4];
+};
+
+int main(void)
+{
+    int i;
+    struct Student chloeInfo = {12345, {90.5f,43.5f,80.0f,90.0f}};
+
+    printf("%d\n", chloeInfo.studentNum);
+    
+    for ( i=0, i < 4, i++;)
+    { 
+        printf("%.2f\n", chloeInfo.grades[i]);
+    }
+
+    return 0;
+
+}
